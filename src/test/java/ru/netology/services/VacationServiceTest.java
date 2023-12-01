50b22cbad21867c7b9a38c92134e07cbb3aecb23
Anastasiia Fokina <stasechka11@yandex.ru>
2023-12-01 16:56:07 +0300

    Changed VacationServiceTest to parameterized

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -2,14 +2,17 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
-    @Test
-    void shouldCalculateVacationCount() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/vacation.csv")
+    void shouldCalculateVacationCount(int expected, int income, int expenses, int threshold) {
         VacationService vacationService = new VacationService();
 
-        int expected = 2;
-        int actual = vacationService.calculate(100_000, 60_000, 150_000);
+        //  int expected = 2;
+        int actual = vacationService.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
